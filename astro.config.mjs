c6db6b3869303c2992c460653fa95f4486031914
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2025-01-08 14:08:00 +0100

    Fjerner CDN

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   base: "/mine-saker", // The base path of your site. Useful for projects hosted under subpaths.
-  build: {
-    assetsPrefix: "https://cdn.nav.no/min-side/tms-dokumentarkiv-redirect", // Change this to your CDN prefix
-  },
   output: "server",
   adapter: node({
     mode: "standalone",
