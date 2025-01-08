9cd42cd4cacd2360f3507d70638388721ea0e1bf
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2025-01-08 14:14:35 +0100

    Oppdatert login-url

--- src/middleware/urls.ts
+++ src/middleware/urls.ts
@@ -1,10 +1,10 @@
 import { getEnvironment } from "@src/utils/server/urls.ts";
 
 const REDIRECT_URI = {
-  local: "http://localhost:4321/tms-dokumentarkiv-redirect",
-  development: "https://www.ansatt.dev.nav.no/tms-dokumentarkiv-redirect",
-  production: "https://www.nav.no/minside/tms-dokumentarkiv-redirect",
+  local: "http://localhost:4321/mine-saker",
+  development: "https://www.ansatt.dev.nav.no/mine-saker",
+  production: "https://www.nav.no/minside/mine-saker",
 };
 
 export const redirectUri = REDIRECT_URI[getEnvironment()];
-export const loginUrl = `/tms-dokumentarkiv-redirect/oauth2/login?redirect=${redirectUri}`;
+export const loginUrl = `/mine-saker/oauth2/login?redirect=${redirectUri}`;
